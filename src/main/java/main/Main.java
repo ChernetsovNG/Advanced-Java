7d842c219769b05211306d202e355e81fe8602e7
ChernetsovNG <n.chernetsov86@gmail.com>
2017-04-14 22:42:50 +0300

    Add UserProfile

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import frontend.SignInServlet;
+
+import javax.servlet.Servlet;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args.length == 1) {
+            String portString = args[0];
+            port = Integer.valueOf(portString);
+        }
+
+        System.out.append("Starting at port: ").append(String.valueOf(port)).append('\n');
+
+        AccountService accountService = new AccountService();
+
+        Servlet signIn = new SignInServlet(accountService);
+    }
+}
